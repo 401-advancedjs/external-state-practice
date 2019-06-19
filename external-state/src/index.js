33b56954cfdca2ab853a0331b749a733dd5069f6
Melissa Stock <melissastock44@gmail.com>
2019-06-19 16:38:30 -0700

    refactored form into component

--- external-state/src/index.js
+++ external-state/src/index.js
@@ -1,28 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-class Form extends React.Component{
-
-  handleChange = event => {
-    event.preventDefault();
-    let name = event.target.value;
-    this.props.handleSubmit(name);
-  }
-
-  render(){
-    return (
-      <form>
-        <label>
-          Name:
-        </label>
-        <input type="text" onChange={ this.handleChange }></input>
-        <input type="submit" onClick={ this.props.handleCount }></input>
-      </form>
-    );
-  }
-}
-
+import Form from './component/Form.js'
 
 class App extends React.Component {
   constructor(props){
